be6b769032d8be57a6e054868f35284303a13818
Tzara <tzarame@gmail.com>
2024-01-31 21:17:04 +0200

    update

--- Homework_3/HW_3-1.py
+++ Homework_3/HW_3-1.py
@@ -1,19 +1,27 @@
-#inputs 
-
-#number_1 = input("Enter fist number: ")
-#number_1 = int(number_1)
-# number_2 = input("Enter second number: ")
-# number_2 = int(number_2)
-
-#debug 
-number_1 = 10
-number_2 = 5
-
-
-operator = input("Enter math operator, + - * / "):
-    if operator == "+":
-        result = number_1 + number_2
-    elif operator == "-":
-        result = number_1 - number_2
-
-print(result)
+#fix for empty string
+result = "Program terminated"
+#inputs
+number_1 = input("Enter fist number: ")
+number_1 = int(number_1)
+number_2 = input("Enter second number: ")
+number_2 = int(number_2)
+operator = input("Enter math operator, + - * / ")
+if input == "quit":
+    quit
+elif operator == "+":
+    result = number_1 + number_2
+    print(number_1, operator, number_2,"=", result)
+elif operator == "-":
+    result = number_1 - number_2
+    print(number_1, operator, number_2,"=", result)
+elif operator == "*":
+    result = number_1 * number_2
+    print(number_1, operator, number_2,"=", result)
+elif operator == "/":
+    if number_2 == 0:
+        result = "Error: divided by zero"
+        print(result)
+        quit
+    else:
+        result = number_1 / number_2
+        print(number_1, operator, number_2,"=", result)
---
+++ Homework_3/HW_3-2.py
@@ -0,0 +1,17 @@
+# input_list = [1, 4, 3, 5, 3, 6, 7]
+input_list = [1]
+list_1 = []
+list_2 = []
+if input_list == []:
+    print(list_1)
+    print(list_2)
+else:
+    list_length = len(input_list)
+    if list_length % list_length == 0:
+        list_half = (list_length // 2) + 1
+    else: 
+        list_half = (list_length // 2)
+    list_1 = input_list[0:list_half]
+    list_2 = input_list[list_half:]
+    print(list_1)
+    print(list_2)
